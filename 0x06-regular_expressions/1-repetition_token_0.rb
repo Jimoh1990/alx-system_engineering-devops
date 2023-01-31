5b390260a752a3454c04b2d41ea4a7ba06a9001c
jimoh1990 <jimohsikiru5@gmail.com>
2023-01-31 11:13:27 -0800

    I am done with the project on regular expression

---
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -0,0 +1,4 @@
+#!/usr/bin/env ruby
+# A regular expresion that matches certaim pathern
+
+puts ARGV[0].scan(/hbt{2,5}n/).join
---
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -0,0 +1,4 @@
+#!/usr/bin/usr ruby
+# Regular expressoon that matches a certain given pathern
+
+puts ARGV[0].scan(/hb?tn/).join
