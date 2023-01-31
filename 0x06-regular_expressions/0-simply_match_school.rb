c651fa98e5c3f8017e79b39d96d6e10b14ac13d0
jimoh1990 <jimohsikiru5@gmail.com>
2023-01-31 11:48:31 -0800

    This is all about the project

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,10 +1,4 @@
 #!/usr/bin/env ruby
 # A regular expression that is simply matching School
 
-input = ARGV[0]
-
-if input.match(/School/)
-  puts input.match(/School/)[0]
-else
-  puts ""
-end
+puts ARGV[0].scan(/School/).join
