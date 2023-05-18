471ae909701810e7cce9eee61f80da098358fdd2
jimoh1990 <jimohsikiru5@gmail.com>
2023-05-18 04:28:27 -0700

    debugging why lots of failed request occur on webserver

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,12 @@
+# This manifest increases the ULIMIT of the default file
+
+exec { 'increase-ulimit-for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# Restart Nginx
+-> exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
